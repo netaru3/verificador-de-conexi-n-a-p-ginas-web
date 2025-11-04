82e7a342ed4dbd08a268931c7e2771a5b342d0ca
Aarón <netaru3@gmail.com>
2025-11-04 20:00:34 -0300

    Add files via upload

---
+++ start-siempre.js
@@ -0,0 +1,7 @@
+import { execSync } from 'child_process'
+function instalar(paquete){try{require.resolve(paquete)} catch{
+execSync(`npm install ${paquete}`)
+}};
+instalar("-g pm2-windows-startup")
+execSync("pm2-startup install")
+ execSync("npx pm2 save")
