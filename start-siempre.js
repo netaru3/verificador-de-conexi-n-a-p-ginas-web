a1bc69e29f229b2516a1528798251e92941c91fa
Aarón <netaru3@gmail.com>
2025-11-05 14:20:48 -0300

    Fix typo and add node start command

--- start-siempre.js
+++ start-siempre.js
@@ -3,5 +3,7 @@ function instalar(paquete){try{require.resolve(paquete)} catch{
 execSync(`npm install ${paquete}`)
 }};
 instalar("-g pm2-windows-startup")
+excecSync("node start")
 execSync("pm2-startup install")
+
  execSync("npx pm2 save")
