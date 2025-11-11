ada585163da287edd09803629f18b8061624a2e2
Aarón <netaru3@gmail.com>
2025-11-11 14:13:13 -0300

    Update start.js

--- start.js
+++ start.js
@@ -1,7 +1,15 @@
+#!/usr/bin/env node
+import path from "path";
+import { fileURLToPath } from "url";;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 
 import { execSync } from 'child_process'
 function instalar(paquete){try{require.resolve(paquete)} catch{
 execSync(`npm install ${paquete}`)
 }};
 instalar("-g pm2")
-execSync("npx pm2 start ecosystem.config.cjs")
+execSync(`pm2 start "${path.join(__dirname, "ecosystem.config.cjs")}"`);
