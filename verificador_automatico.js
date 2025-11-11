673e548a6e468eda1b3ff3827e1fca5c2150b575
Aarón <netaru3@gmail.com>
2025-11-11 14:14:59 -0300

    Update verificador_automatico.js

--- verificador_automatico.js
+++ verificador_automatico.js
@@ -2,30 +2,27 @@ import {logs} from './logs.js'
 import readline from 'readline/promises'
 import {enviargmail} from './enviargmail.js'
 import {codigo} from './enviargmail.js'
-import {gmail} from './enviargmail.js';;
+import {gmail} from './enviargmail.js';
 let tiempo= 30000
-console.log("hola")
+let i=0
+setInterval(() => {
+  i=i+10; if(i===86400){logs.deletepath()}
+}, 10000);
 async function verificador(url) {
 
  setInterval(async () => {
   try{let res=await fetch(url);
     if(!res.ok){;enviargmail(`hubo un error en la conexión con la url
       ${url}`, true,codigo,gmail,gmail,"error de conexión"
-    );logs.savelogs("error de conexión con la url", "high")
+    );logs.savelogs("error de conexión con la url","high")
     ; return false
  }; logs.crearpath();
- logs.savelogs("la conexión es estable","low");return true
-}catch(error){logs.savelogs("error de conexion","high");enviargmail("hubo un error en:"+url+error,
+ logs.savelogs("la conexión es estable","high");return true
+}catch(error){logs.savelogs("error");enviargmail("hubo un error en:"+url+error,
   true,codigo,gmail,gmail,"error en el servidor");return false}
  }, tiempo);
  ;
  }
-let url= "https://www.google.com"
+let url= process.env.url
  verificador(url)
-
  console.log(url)
-
-
-
-
-
